abf97bf2055797d99a82c2dfe82e68e69ba08433
Alexander Korniichuk <world.kor.films@gmail.com>
2021-10-19 10:42:56 +0300

    import/export are included to jest

--- src/App/getObject.js
+++ src/App/getObject.js
@@ -1,7 +1,6 @@
-const getObject = (...args) => {
+export const getObject = (...args) => {
     return args.reduce((accum, current, i) => {
         accum[current] = current
         return accum
     }, {})
 }
-module.exports = getObject
--- src/App/getObject.test.js
+++ src/App/getObject.test.js
@@ -1,4 +1,4 @@
-const getObject = require("./getObject")
+import {getObject} from "./getObject";
 
 test("getObject returns {hi: \"hi\", my: \"my\"}", () => {
     expect(getObject("hi", "my")).toEqual({hi: "hi", my: "my"})
