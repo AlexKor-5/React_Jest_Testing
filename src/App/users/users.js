c61c8b9c8343552887d36d787dacfdb35623b8c7
Alexander Korniichuk <world.kor.films@gmail.com>
2021-10-25 18:07:50 +0300

    fetch mocking example

--- src/App/users/users.js
+++ src/App/users/users.js
@@ -1,11 +1,13 @@
-const getUser = async (number) => {
+import regeneratorRuntime from "regenerator-runtime";
+
+export const getUser = async (number) => {
     try {
         const response = await fetch(`https://jsonplaceholder.typicode.com/users/${number}`)
         return await response.json()
     } catch (e) {
-        console.error(e);
+        // console.error(e);
         return null
     }
 }
 
-export {getUser}
+
--- src/App/users/users.test.js
+++ src/App/users/users.test.js
@@ -1,18 +1,45 @@
 import {getUser} from "./users";
-import { enableFetchMocks } from 'jest-fetch-mock'
+import {enableFetchMocks} from 'jest-fetch-mock'
+
 enableFetchMocks()
 
+// youTube : https://www.youtube.com/watch?v=yhUep7E9O20&ab_channel=LeighHalliday
+// Tutorial
 
+beforeEach(() => {
+    fetch.resetMocks()
+})
 
-// global.fetch = jest.fn(() =>
-//     Promise.resolve({
-//         json: () => Promise.resolve({name: "Alex Kor"})
-//     })
-// )
-
-it("returns user", async () => {
+it("returns user", async () => { //resolve
     fetch.mockResponseOnce(JSON.stringify({name: "Alex Kor"}))
     const acceptedUser = await getUser(1)
     console.log(acceptedUser);
+
+    expect(fetch).toHaveBeenCalledTimes(1)
     expect(acceptedUser.name).toBe("Alex Kor")
+})
+
+it("catches an error", async () => { // reject
+    fetch.mockReject(() => "Failure")
+    const acceptedUser = await getUser(1)
+    console.log(acceptedUser)
+
+    expect(fetch).toHaveBeenCalledTimes(1)
+    expect(acceptedUser).toBe(null)
+})
+
+it("fetches multiple times", async () => {
+    fetch.mockResponses(
+        [
+            JSON.stringify([{name: "Alex Korr"}]),
+            {status: 200}
+        ],
+        [
+            JSON.stringify([{name: "Mike Kor"}]),
+            {status: 200}
+        ]
+    )
+    const acceptedUser = await getUser(1)
+    console.log(acceptedUser);
+
 })
